f894fac06bc55b34614f07f888e4689b3cc48da1
Christian <eyubechristian@gmail.com>
2023-04-21 14:26:18 +0100

    Configured some reusable components

---
+++ src/app/components/SignInButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { FC, useState } from "react";
+import Button from "./ui/Button";
+import { signIn } from "next-auth/react";
+
+interface SignInButtonProps {}
+
+const SignInButton: FC<SignInButtonProps> = ({}) => {
+  const [isloading, setIsloading] = useState<boolean>(false);
+
+  const signInWithGoogle = async () => {
+    setIsloading(true);
+    try {
+      await signIn("google");
+    } catch (error) {
+      toast({
+        title: "Error signing in",
+        message: "Please try again",
+        type: "error",
+      });
+    }
+  };
+
+  return (
+    <Button onClick={signInWithGoogle} isLoading={isloading}>
+      Sign In
+    </Button>
+  );
+};
+
+export default SignInButton;
